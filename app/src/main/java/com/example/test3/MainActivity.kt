fbe0018be6a8cab7b2c2dc0e01fb90465e61278a
tusharyadav <tushar.yadav@csu.fullerton.edu>
2023-09-16 01:25:06 -0700

    added functionality to see intermediate steps

--- app/src/main/java/com/example/test3/MainActivity.kt
+++ app/src/main/java/com/example/test3/MainActivity.kt
@@ -23,6 +23,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var unsortedListTextView: TextView
     private lateinit var inputNumberEditText: EditText
     private lateinit var sortedListTextView: TextView
+    private lateinit var intermediateStepsTextView: TextView
+    private lateinit var stepsTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,10 +33,13 @@ class MainActivity : AppCompatActivity() {
         unsortedListTextView = findViewById(R.id.unsortedListTextView)
         inputNumberEditText = findViewById(R.id.inputNumberEditText)
         sortedListTextView = findViewById(R.id.sortedListTextView)
+        intermediateStepsTextView = findViewById(R.id.intermediateStepsTextView)
+        stepsTextView = findViewById(R.id.stepsTextView)
 
         val addButton = findViewById<Button>(R.id.addButton)
         val sortButton = findViewById<Button>(R.id.sortButton)
-        val clearButton = findViewById<Button>(R.id.clearButton) // Add this line
+        val clearButton = findViewById<Button>(R.id.clearButton)
+
 
         addButton.setOnClickListener { addNumber() }
         sortButton.setOnClickListener { insertionSort() }
@@ -46,6 +51,7 @@ class MainActivity : AppCompatActivity() {
         numbers.clear()
         unsortedListTextView.text = "Unsorted List:"
         sortedListTextView.text = "Sorted List:"
+        clearIntermediateSteps()
     }
 
     private fun addNumber() {
@@ -58,15 +64,29 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun insertionSort() {
+        val steps = mutableListOf<String>() // Add this line to store intermediate steps
         for (i in 1 until numbers.size) {
             val key = numbers[i]
             var j = i - 1
             while (j >= 0 && numbers[j] > key) {
                 numbers[j + 1] = numbers[j]
                 j--
+                steps.add("Step $i: ${numbers.joinToString(", ")}") // Record intermediate steps
             }
             numbers[j + 1] = key
         }
+
+        displayIntermediateSteps(steps) // Display intermediate steps
         sortedListTextView.text = "Sorted List: ${numbers.joinToString(", ")}"
+    }
+
+    private fun displayIntermediateSteps(steps: List<String>) {
+        intermediateStepsTextView.visibility = View.VISIBLE
+        stepsTextView.text = steps.joinToString("\n")
+    }
+
+    private fun clearIntermediateSteps() {
+        intermediateStepsTextView.visibility = View.GONE
+        stepsTextView.text = ""
     }
 }
